8249c827bd41c61e8ad27c836fdab4eccfe0ba05
Tomas Lajda <tomas.lajda0@gmail.com>
2023-07-18 23:14:55 +0200

    recipe app made with js

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f0ff3c96-031e-4865-929d-6ecdc6d59655';
+export const MODAL_CLOSE_SEC = 2.5;
